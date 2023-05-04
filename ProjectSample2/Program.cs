b0ec3be82eb3bfee12cc057f8d84a3fd3e9d6f08
Joan López López <detoslaos@outlook.com>
2023-05-04 07:43:33 +0200

    Create sample projects to test

---
+++ ProjectSample2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectSample2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ SampleProject1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SampleProject1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
