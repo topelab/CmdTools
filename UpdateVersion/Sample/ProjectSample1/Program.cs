c80e6fe851c780982be1116d87876b9cd9488c17
Juan López López <detoslaos@outlook.com>
2023-05-05 19:50:46 +0200

    Set sample projects outside solution

---
+++ UpdateVersion/Sample/ProjectSample1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectSample1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
