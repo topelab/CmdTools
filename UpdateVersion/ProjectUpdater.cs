35a4fb1e3d3ba75a8611071df71b3abe6fa7c950
Joan López López <detoslaos@outlook.com>
2023-07-13 18:23:43 +0200

    Version 1.0.3 - Manage MAUI project version

--- UpdateVersion/ProjectUpdater.cs
+++ UpdateVersion/ProjectUpdater.cs
@@ -8,12 +8,15 @@ namespace UpdateVersion
     {
         private const string VersionNodeName = "Version";
         private const string PropertyGroupNodeName = "PropertyGroup";
+        private const string UseMaui = "UseMaui";
+        private const string MauiVersionNodeName = "ApplicationDisplayVersion";
 
         public void Update(string file, string version)
         {
             XDocument document = XDocument.Load(file);
 
-            var node = document.Descendants().Where(d => d.Name.LocalName == VersionNodeName).FirstOrDefault();
+            var versionNodeName = GetVersionNodeName(document);
+            var node = document.Descendants().Where(d => d.Name.LocalName == versionNodeName).FirstOrDefault();
             if (node != null)
             {
                 node.Value = version;
@@ -21,11 +24,23 @@ namespace UpdateVersion
             else
             {
                 var firstPropertyGroup = document.Descendants().Where(d => d.Name.LocalName == PropertyGroupNodeName).First();
-                var versionElement = new XElement(VersionNodeName, version);
+                var versionElement = new XElement(versionNodeName, version);
                 firstPropertyGroup.Add(versionElement);
             }
 
             File.WriteAllText(file, document.ToString());
+        }
+
+        private string GetVersionNodeName(XDocument document)
+        {
+            var result = VersionNodeName;
+
+            if (document.Descendants().Any(d => d.Name.LocalName == UseMaui && d != null && d.Value.Equals("true", System.StringComparison.CurrentCultureIgnoreCase)))
+            {
+                result = MauiVersionNodeName;
+            }
+
+            return result;
         }
     }
 }
